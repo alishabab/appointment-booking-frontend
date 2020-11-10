b6a6befc0dc4fc1430d3dec8cef8f29357e4c34c
Shabab <shababsaifi@gmail.com>
2020-11-10 18:45:23 +0530

    added method to get list of doctors

---
+++ src/services/user.service.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'https://frozen-river-95471.herokuapp.com/';
+
+const getDoctors = () => axios.get(`${API_URL}api/v1/doctors`, { headers: authHeader() });
+
+export default {
+  getDoctors,
+};
