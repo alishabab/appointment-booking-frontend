997aa8a7e8d9ebb35d4c9ace407174730f12886d
Shabab <shababsaifi@gmail.com>
2020-11-11 22:16:05 +0530

    fixed bug for not rendering component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  Router, Switch, Route, Link,
+  BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -46,6 +46,11 @@ const App = () => {
           {currentUser ? (
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
+                <Link to="/profile" className="nav-link">
+                  {currentUser.user.name}
+                </Link>
+              </li>
+              <li className="nav-item">
                 <a href="/login" className="nav-link" onClick={logOut}>
                   LogOut
                 </a>
@@ -68,7 +73,7 @@ const App = () => {
         </nav>
         <div className="container mt-3">
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path={['/', '/home']} component={Login} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/profile" component={Profile} />
